ec30c012f175e39880d27e23b9c01257380aed87
Shivanshu <sb652515@gmail.com>
2024-11-25 11:47:54 +0530

    initial commit

--- models/PoojaCategory.js
+++ models/PoojaCategory.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const poojaCategorySchema = new mongoose.Schema({
-    category: { type: String, required: true },
-    pooja_image: { type: String, required:true},
-    short_discription:{type: String,require:true},
-    long_discription:{type: String , require:true},
+    category: { type: String },
+    pooja_image: { type: String},
+    short_discription:{type: String},
+    long_discription:{type: String },
     status: { type: String, default:'active' },
     updated_at:{ type: Date,default:Date.now},
     created_at: { type: Date, default: Date.now },
